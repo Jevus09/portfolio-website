b3a8803a040c39185038280a916383acfc1790a0
jevus09 <jevus09@gmail.com>
2023-04-06 21:28:22 -0500

    Updated header section added typewriter effect feature

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,106 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import './header.css'
 import CTA from './cta'
-import ME from '../../assets/ME.png'
+import Typewriter from 'typewriter-effect';
 import './HeaderSocials'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
+
+  const [showSecondTypewriter, setShowSecondTypewriter] = useState(false)
+  const [showThirdTypewriter, setShowThirdTypewriter] = useState(false)
+
+  useEffect(() =>{
+    setTimeout(() => {
+      setShowSecondTypewriter(true)
+    }, 5400)
+  }, [])
+
+
+  useEffect(() =>{
+    setTimeout(() => {
+      setShowThirdTypewriter(true)
+    }, 9600)
+  }, [])
+
   return (
     <header>
       <div className=" container header_container">
-        <h5>Hello I'm</h5>
-        <h1>Jesus Calderon</h1>
-        <h5 className="text-light">Software Developer</h5>
         <CTA />
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString(`Hello, World!`)
+              .callFunction(() => {
+              })
+              .pauseFor(2500)
+              .callFunction(() => {
+              })
+              .start();
+          }} 
+          options={
+            {
+              cursor: '',
+            }}
+          />
+          {showSecondTypewriter && (<Typewriter
+          onInit={(typewriter1) => {
+            typewriter1.typeString(`My name is Jesus Calderon`)
+              .callFunction(() => {
+              })
+              .pauseFor(2500)
+              .callFunction(() => {
+              })
+              .start();
+          }}
+          options={
+            {
+              cursor: '',
+            }}
+          />)}
+        {showThirdTypewriter && (<Typewriter
+          onInit={(typewriter2) => {
+            typewriter2.typeString('I build things for the web.')
+              .callFunction(() => {
+              })
+              .pauseFor(2500)
+              .deleteChars(12)
+              .pauseFor(500)
+              .typeString('with ReactJS')
+              .pauseFor(1500)
+              .deleteChars(7)
+              .pauseFor(500)
+              .typeString('Python')
+              .pauseFor(1500)
+              .deleteChars(6)
+              .pauseFor(500)
+              .typeString('CSS-3')
+              .pauseFor(1500)
+              .deleteChars(5)
+              .pauseFor(500)
+              .typeString('BootStrap')
+              .pauseFor(1500)
+              .deleteChars(9)
+              .pauseFor(500)
+              .typeString('HTML-5')
+              .pauseFor(1500)
+              .deleteChars(6)
+              .pauseFor(500)
+              .typeString('JavaScript')
+              .callFunction(() => {
+              })
+              .start();
+              
+          }}
+          options={
+            {
+              loop: false,
+              autoStart: true,
+              cursor: '',
+            }
+            
+          }
+          />)}
         <HeaderSocials />
-        <div className='me'>
-          <img src={ME} alt='me' />
-        </div>
         <div className='header_email'>
           <a href="mailto:jevus09@gmail.com" rel="noopener noreferrer" target="_blank">Jevus09@gmail.com</a>
         </div>
