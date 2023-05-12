05df3939eb6fc4ad3e92e1ae9306eb4351a06596
jevus09 <jevus09@gmail.com>
2023-05-12 15:56:11 -0500

    added portfolio item

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,64 +2,94 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/exmple.PNG'
 import Crypto from '../../assets/Crypto.PNG'
+import ELE from '../../assets/electronix.PNG'
 import { FaReact } from 'react-icons/fa'
 import { FaCss3Alt } from 'react-icons/fa'
 import { TbBrandFirebase } from 'react-icons/tb'
-
+import { DiMongodb } from 'react-icons/di'
+import { SiExpress } from 'react-icons/si'
+import { GrNode } from 'react-icons/gr'
 
 const data = [
   {
     id: 1,
     image: Crypto,
     title: 'Crypto Central',
-    language: <><FaReact /> <FaCss3Alt /> <TbBrandFirebase /></>,
+    language: (
+      <>
+        <FaReact /> <FaCss3Alt /> <TbBrandFirebase />
+      </>
+    ),
     github: 'https://github.com/Jevus09/crypto-central',
     demo: 'https://crypto-central-01.netlify.app/',
-    description: 'This is a crypto website that utilizes the Coingecko API to provide users with real-time information about cryptocurrency prices and market trends. Built with Firebase as the back-end infrastructure, ReactJS for the front-end framework, and Material UI for a sleek and intuitive user interface'
+    description:
+      'This is a crypto website that utilizes the Coingecko API to provide users with real-time information about cryptocurrency prices and market trends. Built with Firebase as the back-end infrastructure, ReactJS for the front-end framework, and Material UI for a sleek and intuitive user interface',
   },
   {
     id: 2,
-    image: IMG1,
-    title: 'Example title 2',
-    language: <><FaReact /> <FaCss3Alt /></>,
-    github: 'https://github.com/Jevus09',
-    demo: 'https://github.com/Jevus09',
-    description: 'This is a crypto website that utilizes the Coingecko API to provide users with real-time information about cryptocurrency prices and market trends. Built with Firebase as the back-end infrastructure, ReactJS for the front-end framework, and Material UI for a sleek and intuitive user interface'
+    image: ELE,
+    title: 'Electronix Hub',
+    language: (
+      <>
+        <FaReact /> <DiMongodb />
+        <SiExpress /> <GrNode />
+      </>
+    ),
+    github: 'https://github.com/Jevus09/electronix-hub',
+    demo: 'https://electronixhub.herokuapp.com/',
+    description:
+      'MERN eCommerce website focused on tech products. Leveraging MongoDB, Express.js, React.js, and Node.js, I created a seamless user experience, robust backend functionality, and secure payment integration. This platform showcases my expertise in crafting modern, responsive eCommerce solutions.',
   },
-  {
-    id: 3,
-    image: IMG1,
-    title: 'Example title 3',
-    language: <><FaReact /> <FaCss3Alt /></>,
-    github: 'https://github.com/Jevus09',
-    demo: 'https://github.com/Jevus09',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    id: 4,
-    image: IMG1,
-    title: 'Example title 4',
-    language: <><FaReact /> <FaCss3Alt /></>,
-    github: 'https://github.com/Jevus09',
-    demo: 'https://github.com/Jevus09',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  }
+  // {
+  //   id: 3,
+  //   image: IMG1,
+  //   title: 'Example title 3',
+  //   language: (
+  //     <>
+  //       <FaReact /> <FaCss3Alt />
+  //     </>
+  //   ),
+  //   github: 'https://github.com/Jevus09',
+  //   demo: 'https://github.com/Jevus09',
+  //   description:
+  //     'This is a crypto website that utilizes the Coingecko API to provide users with real-time information about cryptocurrency prices and market trends. Built with Firebase as the back-end infrastructure, ReactJS for the front-end framework, and Material UI for a sleek and intuitive user interface',
+  // },
+  // {
+  //   id: 4,
+  //   image: IMG1,
+  //   title: 'Example title 4',
+  //   language: (
+  //     <>
+  //       <FaReact /> <FaCss3Alt />
+  //     </>
+  //   ),
+  //   github: 'https://github.com/Jevus09',
+  //   demo: 'https://github.com/Jevus09',
+  //   description:
+  //     'This is a crypto website that utilizes the Coingecko API to provide users with real-time information about cryptocurrency prices and market trends. Built with Firebase as the back-end infrastructure, ReactJS for the front-end framework, and Material UI for a sleek and intuitive user interface',
+  // },
 ]
 
-
-
-
-const Portfolio = () => {
+const Portfolio  = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className='container portfolio_container'>
-        {
-          data.map(({ id, image, title, github, demo, language, language2, language3, description }) => {
+        {data.map(
+          ({
+            id,
+            image,
+            title,
+            github,
+            demo,
+            language,
+            description,
+          }) => {
             return (
               <article key={id} className='portfolio_item'>
+                <div className='item'>
                 <div className='portfolio_item-image'>
                   <img src={image} alt={title} />
                 </div>
@@ -71,17 +101,31 @@ const Portfolio = () => {
                       <h4>{language}</h4>
                     </div>
                     <div className='portfolio_item-cta'>
-                      <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-                      <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+                      <a
+                        href={demo}
+                        className='btn btn-primary'
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        Live Demo
+                      </a>
+                      <a
+                        href={github}
+                        className='btn'
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        Github
+                      </a>
                     </div>
                   </div>
                 </div>
+                </div>
               </article>
             )
-          })
-        }
+          }
+        )}
       </div>
-
     </section>
   )
 }
