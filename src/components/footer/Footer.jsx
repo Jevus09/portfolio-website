47c9de1972c26e8ce5c9e1000f26f81dce54b2bb
jevus09 <jevus09@gmail.com>
2024-10-03 22:26:44 -0500

    updated href issues

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import LOGO from '../../assets/logo.PNG'
 const Footer = () => {
   return (
     <footer>
-      <a href='#' className='footer_logo'><img src={LOGO} alt='logo'></img></a>
+      <a href='/home' className='footer_logo'><img src={LOGO} alt='logo'></img></a>
       <div className="footer_copyright">
         <small> Copyright &copy; 2022 Jesus Calderon. All rights reserved</small>
       </div>
